cb09b41033ff17776543c9687cd9c9c7ca080671
zolphinus <jonnyboy2167@yahoo.com>
2014-08-28 23:13:09 -0400

    Created Chopstick Starting Files

---
+++ Chopstick/chopstick.cpp
@@ -0,0 +1,20 @@
+#ifndef CHOPSTICK
+#define CHOPSTICK
+
+class Chopstick{
+    Chopstick();
+
+
+
+private:
+
+    //variables
+    bool isRunning;
+
+    //methods
+    void initOS(){
+        isRunning = true;
+    }
+};
+
+#endif // CHOPSTICK
